8ab52ff03857df4eb0b854502cdbedd44e6d70d5
Sumit Pathak <sumit.pathak0606@gmail.com>
2024-04-21 14:46:00 +0530

    state handling at auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:twitch_clone/firebase_options.dart';
 import 'package:twitch_clone/providers/user_provider.dart';
+import 'package:twitch_clone/resources/auth_methods.dart';
 import 'package:twitch_clone/screens/login_screen.dart';
 import 'package:twitch_clone/screens/onboarding_screen.dart';
 import 'package:twitch_clone/screens/signup_screen.dart';
 import 'package:twitch_clone/util/colors.dart';
+import 'package:twitch_clone/widgets/loading_indicator.dart';
+import 'model/user.dart' as model;
 
 import 'screens/home_screen.dart';
 
@@ -24,25 +28,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Real time Streaming App',
-        theme: ThemeData.light().copyWith(
-            scaffoldBackgroundColor: backgroundColor,
-            appBarTheme: AppBarTheme.of(context).copyWith(
-                backgroundColor: backgroundColor,
-                elevation: 0,
-                titleTextStyle: const TextStyle(
-                  color: primaryColor,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                ),
-                iconTheme: const IconThemeData(color: primaryColor))),
-        routes: {
-          OnboardingScreen.routeName: (context) => const OnboardingScreen(),
-          LoginScreen.routeName: (context) => const LoginScreen(),
-          SignUpScreen.routeName: (context) => const SignUpScreen(),
-          HomeScreen.routeName: (context) => const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+      title: 'Real time Streaming App',
+      theme: ThemeData.light().copyWith(
+          scaffoldBackgroundColor: backgroundColor,
+          appBarTheme: AppBarTheme.of(context).copyWith(
+              backgroundColor: backgroundColor,
+              elevation: 0,
+              titleTextStyle: const TextStyle(
+                color: primaryColor,
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+              iconTheme: const IconThemeData(color: primaryColor))),
+      routes: {
+        OnboardingScreen.routeName: (context) => const OnboardingScreen(),
+        LoginScreen.routeName: (context) => const LoginScreen(),
+        SignUpScreen.routeName: (context) => const SignUpScreen(),
+        HomeScreen.routeName: (context) => const HomeScreen(),
+      },
+      home: FutureBuilder(
+        future: AuthMethods()
+            .getCurrentUser(FirebaseAuth.instance.currentUser != null
+                ? FirebaseAuth.instance.currentUser!.uid
+                : null)
+            .then((value) {
+          if (value != null) {
+            Provider.of<UserProvider>(context, listen: false)
+                .setUser(model.User.fromMap(value));
+          }
+          return value;
+        }),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const LoadingIndicator();
+          }
+          if (snapshot.hasData) {
+            return const HomeScreen();
+          }
+          return const OnboardingScreen();
         },
-        home: const OnboardingScreen());
+      ),
+    );
   }
 }
